b7185b419a88694a81c64b7cec33ba27526bc126
vlad-henk <vladsukhenko1539@gmail.com>
2023-08-20 14:55:19 +0300

    Validation!

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+  test "product attributes must not be empty" do 
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any? 
+    assert product.errors[:price].any? 
+    assert product.errors[:image_url].any?
+  end
+
+  test "product is not valid without a unique title" do 
+    product = Product.new(title: products(:ruby).title,
+    description: "yyy", 
+    price: 1, 
+    image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title] 
+  end
 end
