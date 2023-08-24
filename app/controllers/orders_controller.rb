40eb2b10a9e0dd519be8e99804d7c7ebcd70016a
vlad-henk <vladsukhenko1539@gmail.com>
2023-08-24 18:55:30 +0300

    Sending Emails and Processing Payments Efficiently

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -31,6 +31,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
+        ChargeOrderJob.perform_later(@order,pay_type_params.to_h)
         format.html { redirect_to store_index_url, notice:
           'Thank you for your order.' }
         format.html { redirect_to order_url(@order), notice: "Order was successfully created." }
@@ -79,6 +80,18 @@ class OrdersController < ApplicationController
     def ensure_cart_isnt_empty
       if @cart.line_items.empty?
       redirect_to store_index_url, notice: 'Your cart is empty'
+      end 
+    end
+
+    def pay_type_params
+      if order_params[:pay_type] == "Credit card"
+        params.require(:order).permit(:credit_card_number, :expiration_date) 
+      elsif order_params[:pay_type] == "Check"
+        params.require(:order).permit(:routing_number, :account_number) 
+      elsif order_params[:pay_type] == "Purchase order"
+        params.require(:order).permit(:po_number) 
+      else
+        {}
       end 
     end
 end
